59ca337414c72d20759850dadc7a0cb367475f17
patrick chang <p.jloveshiphop@gmail.com>
2020-02-26 13:15:53 +0900

    updated settings second time

--- django_project/settings.py
+++ django_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&gmi7^hwr4brv&j8-1^gcn5%&147jm#1r3yfgvo25pz**kwf5!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['fakeblogdjangoapp.herokuapp.com']
 
