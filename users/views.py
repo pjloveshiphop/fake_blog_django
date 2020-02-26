702d345f0c5abb74a59889b56efb5e4a132cdea8
patrick chang <p.jloveshiphop@gmail.com>
2020-02-26 14:02:21 +0900

    set debug value

--- users/views.py
+++ users/views.py
@@ -10,7 +10,7 @@ def register(request):
         if form.is_valid():
             form.save()
             username=form.cleaned_data.get('username')
-            messages.success(request, 'Registartion was successful! Try to log in now!')
+            messages.success(request, 'Registration was successful! Try to log in now!')
             return redirect('login')
     else:
         form = UserRegisterForm()
